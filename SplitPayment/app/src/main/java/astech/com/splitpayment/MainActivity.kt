b84c2ca5d55324d1f0b24a530cfd065f74f2e5c6
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-06-09 18:31:56 -0500

    Add Analitycs Event

--- SplitPayment/app/src/main/java/astech/com/splitpayment/MainActivity.kt
+++ SplitPayment/app/src/main/java/astech/com/splitpayment/MainActivity.kt
@@ -2,10 +2,19 @@ package astech.com.splitpayment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Analitycs Event
+        val analitycs:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analitycs.logEvent("OPEN_APP", bundle)
     }
 }
