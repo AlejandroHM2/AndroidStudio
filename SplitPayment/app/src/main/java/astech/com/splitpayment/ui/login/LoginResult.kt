11805fd387f74dc6dd08509ee7c1ddc07f75be61
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-06-08 20:08:15 -0500

    SplitPayment

---
+++ SplitPayment/app/src/main/java/astech/com/splitpayment/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package astech.com.splitpayment.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
