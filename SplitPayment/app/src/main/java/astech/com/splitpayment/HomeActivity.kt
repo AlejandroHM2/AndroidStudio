a376d827662f4f4fe7cc5662df491289d158006d
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-06-09 23:36:13 -0500

    Add Home view
    Add firebase-auth

---
+++ SplitPayment/app/src/main/java/astech/com/splitpayment/HomeActivity.kt
@@ -0,0 +1,11 @@
+package astech.com.splitpayment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
