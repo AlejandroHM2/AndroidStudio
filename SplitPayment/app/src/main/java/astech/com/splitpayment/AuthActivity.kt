c6554b64e744b331efde27abc8f38645aa1177e6
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-06-09 21:49:36 -0500

    Fix machinstate

--- SplitPayment/app/src/main/java/astech/com/splitpayment/AuthActivity.kt
+++ SplitPayment/app/src/main/java/astech/com/splitpayment/AuthActivity.kt
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity() : AppCompatActivity() {
+class AuthActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
 
         // Analitycs Event
         val analitycs:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
