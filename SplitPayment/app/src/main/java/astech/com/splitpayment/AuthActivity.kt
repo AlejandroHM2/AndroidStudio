f165bc4adab9d0fe118090b2da66a6daf95e72e6
Alejandro Hernandez <jesus.hernandezm@axity.com>
2022-06-10 01:06:51 -0500

    Loging basic hecho

--- SplitPayment/app/src/main/java/astech/com/splitpayment/AuthActivity.kt
+++ SplitPayment/app/src/main/java/astech/com/splitpayment/AuthActivity.kt
@@ -1,8 +1,12 @@
 package astech.com.splitpayment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity() : AppCompatActivity() {
 
@@ -16,5 +20,54 @@ class AuthActivity() : AppCompatActivity() {
         val bundle = Bundle()
         bundle.putString("message", "Integración de Firebase completa")
         analitycs.logEvent("OPEN_APP", bundle)
+
+        //Setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autenticación"
+        signUpButton.setOnClickListener {
+            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),
+                    passwordEditText.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                }
+            }
+        }
+
+        loginButton.setOnClickListener {
+            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(),
+                    passwordEditText.text.toString()).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email:String, provider:ProviderType){
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
